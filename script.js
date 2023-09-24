376b1446e434838a1f1a049de13fcdcd4a9e3bbb
psum <sum.philipp@gmail.com>
2023-09-24 13:06:30 +0200

    finished the calculator with the basic features
    the numbers are not getting rounded yet, if the calculator has to show long floating point numbers.
    Im going to fix this later

--- script.js
+++ script.js
@@ -16,21 +16,81 @@ function divide (a, b) {
 
 function operate (value1, value2, operator){
     switch (operator){
-        case plus:
-            add(value1, value2);
-            break;
-        case minus:
-            substract(value1, value2);
-            break;
-        case multiply:
-            multiply(value1, value2);
-            break;
-        case divide:
-            divide(value1, value2);
-            break;
+        case "+":
+            return add(value1, value2);
+        case "-":
+            return substract(value1, value2);
+        case "x":
+            return multiply(value1, value2);
+        case "/":
+            return divide(value1, value2);
     }
 }
 
-let firstNumber;
-let secondNumber;
-let operator;
+function calculate () {
+    if (firstNumber != "" && secondNumber != ""){
+        firstNumber = parseInt(firstNumber);
+        secondNumber = parseInt(secondNumber);
+        result = operate(firstNumber, secondNumber, operator);
+        updateDisplay(result);
+        firstNumber = result.toString();
+        console.log(firstNumber);
+        // operator = "";
+        secondNumber = "";
+    }
+}
+
+function updateDisplay(text){
+    document.querySelector(".display").innerHTML = text;
+}
+
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let result;
+let displayValue = "Start calculating!";
+let isFirstNumber = true;
+const pattern = /^[\d]+$/;
+const patternOperator = /^[+\-x/]+$/;
+
+const divItems = document.querySelectorAll(".grid-item");
+const clearButton = document.querySelector(".clear");
+const equalsButton = document.querySelector(".equals");
+
+
+clearButton.addEventListener("click", function () {
+    firstNumber = "";
+    secondNumber = "";
+    operator = "";
+    updateDisplay("");
+    isFirstNumber = true;
+})
+
+divItems.forEach(function (div) {
+    div.addEventListener('click', function () {
+
+      if (pattern.test(div.textContent) && isFirstNumber == true) {
+        // is number
+        firstNumber = firstNumber + div.textContent;
+        updateDisplay(firstNumber);}
+
+    if (patternOperator.test(div.textContent)) {
+        // is operator
+        isFirstNumber = false;
+        calculate();
+        operator = div.textContent;
+        updateDisplay(`${firstNumber} ${operator}`)
+
+        }
+    if (pattern.test(div.textContent) && isFirstNumber == false) {
+        // is number
+        secondNumber = secondNumber + div.textContent;
+        updateDisplay(`${firstNumber} ${operator} ${secondNumber}`);}
+          
+    });
+  });
+
+  equalsButton.addEventListener("click", function (){
+    calculate();
+  })
+  
